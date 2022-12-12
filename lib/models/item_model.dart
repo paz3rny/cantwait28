9c8cab9e5b617c68deddf9ca9f758e3ca56472f9
Marcin Eberst <marcin@v-zone.pl>
2022-12-12 20:47:46 +0100

    implement item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel(
+      {required this.id,
+      required this.title,
+      required this.imageURL,
+      required this.releaseDate});
+
+  final String title;
+  final String imageURL;
+  final DateTime releaseDate;
+  final String id;
+}
